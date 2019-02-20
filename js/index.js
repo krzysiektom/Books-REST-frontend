d2905e8b7514f09d1732ea6aa67ebdc786fb0f56
Krzysztof Tomala <krzysiektom@op.pl>
2019-02-20 22:43:10 +0100

    zadanie 5 i 6

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     var mainDiv = $('div')
-    //$(mainDiv).find('div').hide()
-    console.log(mainDiv.children())
     $.ajax({
         url: "http://localhost:8282/books/",
         data: {},
@@ -17,11 +15,10 @@ $(document).ready(function () {
     });
 
     function newLiElement(book) {
-        return $("<li data-id=" + book.id + ">" + book.title + "</li><div style='display: none'><a id='author'></a><br><a id = 'publisher'></a><br><a id='isbn'></a></div>")
+        return $("<li data-id=" + book.id + ">" + book.title + " <a href>Usuń</a></li><div style='display: none'><a id='author'></a><br><a id = 'publisher'></a><br><a id='isbn'></a><br><a id='type'></a></div>")
     }
 
     $(mainDiv).on('click', 'li', function () {
-        console.log($(this).data('id'));
         var nextDiv = $(this).next('div')
         $.ajax({
             url: "http://localhost:8282/books/" + $(this).data('id'),
@@ -31,16 +28,56 @@ $(document).ready(function () {
         }).done(function (book) {
             fillBook(nextDiv, book)
             nextDiv.toggle()
-
         }).fail(function (xhr, status, err) {
         }).always(function (xhr, status) {
         });
     });
 
     function fillBook(nextDiv, book) {
-        console.log(nextDiv.children('a#author'))
         nextDiv.children('a#author').text("Autor: " + book.author)
         nextDiv.children('a#publisher').text("Wydawca: " + book.publisher)
         nextDiv.children('a#isbn').text("Isbn: " + book.isbn)
+        nextDiv.children('a#type').text("Typ: " + book.type)
     }
+
+    $('button').on('click', function () {
+        event.preventDefault()
+        var book = $('form input#title')
+
+        $.ajax({
+            url: "http://localhost:8282/books",
+            data: JSON.stringify({
+                "isbn": $('form input#isbn')[0].value,
+                "title": $('form input#title')[0].value,
+                "publisher": $('form input#publisher')[0].value,
+                "type": $('form input#type')[0].value,
+                "author": $('form input#author')[0].value
+            }),
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function (status) {
+            location.reload();
+        }).fail(function (xhr, status, err) {
+            console.log(err)
+        }).always(function (xhr, status) {
+        });
+    })
+
+    $(mainDiv).on('click', 'a', function () {
+        event.stopPropagation()
+        event.preventDefault()
+
+        $.ajax({
+            url: "http://localhost:8282/books/" + $(this).parent().data('id'),
+            data: {},
+            type: "DELETE",
+            dataType: "json"
+        }).done(function (books) {
+            location.reload();
+        }).fail(function (xhr, status, err) {
+        }).always(function (xhr, status) {
+        });
+    })
+
 });
