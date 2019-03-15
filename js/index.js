be438668adb522f538b94a4e27c2258bdb18088f
Krzysztof <krzysiektom@op.pl>
2019-03-15 20:21:26 +0100

    Warsztaty

--- js/index.js
+++ js/index.js
@@ -1,83 +1,72 @@
 $(document).ready(function () {
+    function makeAjaxCall(type, idOrBook, callback) {
+       
+        if (type === 'POST') {
+            var dataSet = JSON.stringify(idOrBook)
+            var contentTypeSet = "application/json"
+            idOrBook = ''
+        } else {
+            var dataSet = {}
+            var contentTypeSet = ''
+        }
+
+        $.ajax({
+            url: "http://localhost:8282/books/" + idOrBook,
+            data: dataSet,
+            type: type,
+            contentType: contentTypeSet,
+            dataType: "json",
+            success: callback
+        })
+    }
+
     var mainDiv = $('div')
-    $.ajax({
-        url: "http://localhost:8282/books/",
-        data: {},
-        type: "GET",
-        dataType: "json"
-    }).done(function (books) {
+    makeAjaxCall('GET', '', function (books) {
         for (const book of books) {
             var newLi = newLiElement(book);
             mainDiv.append(newLi)
         }
-    }).fail(function (xhr, status, err) {
-    }).always(function (xhr, status) {
-    });
+    })
 
     function newLiElement(book) {
-        return $("<li data-id=" + book.id + ">" + book.title + " <a href>Usuń</a></li><div style='display: none'><a id='author'></a><br><a id = 'publisher'></a><br><a id='isbn'></a><br><a id='type'></a></div>")
+        return $("<li data-id=" + book.id + ">" + book.title + " <a id='delete' href>Usuń</a></li><div style='display: none'><a id='author'></a><br><a id = 'publisher'></a><br><a id='isbn'></a><br><a id='type'></a></div>")
     }
 
     $(mainDiv).on('click', 'li', function () {
-        var nextDiv = $(this).next('div')
-        $.ajax({
-            url: "http://localhost:8282/books/" + $(this).data('id'),
-            data: {},
-            type: "GET",
-            dataType: "json"
-        }).done(function (book) {
-            fillBook(nextDiv, book)
-            nextDiv.toggle()
-        }).fail(function (xhr, status, err) {
-        }).always(function (xhr, status) {
-        });
+        var nextDiv = $(this).next('div');
+        if (nextDiv.is(":hidden")) {
+            makeAjaxCall('GET', $(this).data('id'), function (book) {
+                nextDiv.children('#author').text("Autor: " + book.author)
+                nextDiv.children('#publisher').text("Wydawca: " + book.publisher)
+                nextDiv.children('#isbn').text("Isbn: " + book.isbn)
+                nextDiv.children('#type').text("Typ: " + book.type)
+            })
+        };
+        nextDiv.toggle()
     });
-
-    function fillBook(nextDiv, book) {
-        nextDiv.children('a#author').text("Autor: " + book.author)
-        nextDiv.children('a#publisher').text("Wydawca: " + book.publisher)
-        nextDiv.children('a#isbn').text("Isbn: " + book.isbn)
-        nextDiv.children('a#type').text("Typ: " + book.type)
-    }
 
     $('button').on('click', function () {
         event.preventDefault()
-        var book = $('form input#title')
-
-        $.ajax({
-            url: "http://localhost:8282/books",
-            data: JSON.stringify({
-                "isbn": $('form input#isbn')[0].value,
-                "title": $('form input#title')[0].value,
-                "publisher": $('form input#publisher')[0].value,
-                "type": $('form input#type')[0].value,
-                "author": $('form input#author')[0].value
-            }),
-            type: "POST",
-            contentType: "application/json",
-            dataType: "json"
-        }).done(function (status) {
+        var book = {
+            "isbn": $('form input#isbn')[0].value,
+            "title": $('form input#title')[0].value,
+            "publisher": $('form input#publisher')[0].value,
+            "type": $('form input#type')[0].value,
+            "author": $('form input#author')[0].value
+        }
+        makeAjaxCall('POST', book, function () {
             location.reload();
-        }).fail(function (xhr, status, err) {
-            console.log(err)
-        }).always(function (xhr, status) {
-        });
+        })
     })
 
-    $(mainDiv).on('click', 'a', function () {
+    $(mainDiv).on('click', 'a#delete', function () {
         event.stopPropagation()
         event.preventDefault()
+        event.stopImmediatePropagation()
 
-        $.ajax({
-            url: "http://localhost:8282/books/" + $(this).parent().data('id'),
-            data: {},
-            type: "DELETE",
-            dataType: "json"
-        }).done(function (books) {
+        makeAjaxCall('DELETE', $(this).parent().data('id'), function () {
             location.reload();
-        }).fail(function (xhr, status, err) {
-        }).always(function (xhr, status) {
-        });
+        })
     })
 
 });
