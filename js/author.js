dbf0179c5af534370f56739970c20b2eea13f3be
krzysiektom <krzysiektom@op.pl>
2019-04-22 20:55:33 +0200

    authors page

---
+++ js/author.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    function makeAjaxCall(type, idOrAuthor, callback) {
+
+        if (type === 'POST') {
+            var dataSet = JSON.stringify(idOrAuthor);
+            var contentTypeSet = "application/json";
+            idOrAuthor = ''
+        } else {
+            var dataSet = {};
+            var contentTypeSet = '';
+        }
+
+        $.ajax({
+            url: "http://localhost:8080/authors/" + idOrAuthor,
+            data: dataSet,
+            type: type,
+            contentType: contentTypeSet,
+            dataType: "json",
+            success: callback
+        })
+    }
+
+    var mainDiv = $('div')
+    makeAjaxCall('GET', '', function (authors) {
+        for (const author of authors) {
+            var newLi = newLiElement(author);
+            mainDiv.append(newLi)
+        }
+    });
+
+    function newLiElement(author) {
+        return $("<li data-id=" + author.id + ">" + author.firstName + " " + author.lastName + " <a id='delete' href>Usuń</a></li><div style='display: none'><a id='books'></a></div>")
+    }
+
+    $(mainDiv).on('click', 'li', function () {
+        var nextDiv = $(this).next('div');
+        if (nextDiv.is(":hidden")) {
+            makeAjaxCall('GET', $(this).data('id'), function (author) {
+                var divAuthors = nextDiv.children('#books').text("Książki: ");
+                booksList(author.books, divAuthors);
+            })
+        }
+        nextDiv.toggle()
+    });
+
+    function booksList(books, nextDiv) {
+        for (const book of books) {
+            var newA = newAElementBook(book);
+            nextDiv.append(newA)
+        }
+    }
+
+    function newAElementBook(book) {
+        return $("<a data-id=" + book.id + ">" + book.title + " </a>");
+    }
+
+    $('button').on('click', function () {
+        event.preventDefault();
+        var author = {
+            "firstName": $('form input#firstName')[0].value,
+            "lastName": $('form input#lastName')[0].value,
+        };
+        makeAjaxCall('POST', author, function () {
+            location.reload();
+        })
+    });
+
+    $(mainDiv).on('click', 'a#delete', function () {
+        event.stopPropagation();
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        makeAjaxCall('DELETE', $(this).parent().data('id'), function () {
+            location.reload();
+        })
+    })
+
+});
--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     function makeAjaxCall(type, idOrBook, callback) {
-       
+
         if (type === 'POST') {
-            var dataSet = JSON.stringify(idOrBook)
-            var contentTypeSet = "application/json"
+            var dataSet = JSON.stringify(idOrBook);
+            var contentTypeSet = "application/json";
             idOrBook = ''
         } else {
-            var dataSet = {}
-            var contentTypeSet = ''
+            var dataSet = {};
+            var contentTypeSet = '';
         }
 
         $.ajax({
-            url: "http://localhost:8282/books/" + idOrBook,
+            url: "http://localhost:8080/books/" + idOrBook,
             data: dataSet,
             type: type,
             contentType: contentTypeSet,
@@ -26,43 +26,55 @@ $(document).ready(function () {
             var newLi = newLiElement(book);
             mainDiv.append(newLi)
         }
-    })
+    });
 
     function newLiElement(book) {
-        return $("<li data-id=" + book.id + ">" + book.title + " <a id='delete' href>Usuń</a></li><div style='display: none'><a id='author'></a><br><a id = 'publisher'></a><br><a id='isbn'></a><br><a id='type'></a></div>")
+        return $("<li data-id=" + book.id + ">" + book.title + " <a id='delete' href>Usuń</a></li><div style='display: none'><a id='authors'></a><br><a id = 'publisher'></a><br><a id='isbn'></a><br><a id='type'></a></div>")
     }
 
     $(mainDiv).on('click', 'li', function () {
         var nextDiv = $(this).next('div');
         if (nextDiv.is(":hidden")) {
             makeAjaxCall('GET', $(this).data('id'), function (book) {
-                nextDiv.children('#author').text("Autor: " + book.author)
-                nextDiv.children('#publisher').text("Wydawca: " + book.publisher)
-                nextDiv.children('#isbn').text("Isbn: " + book.isbn)
-                nextDiv.children('#type').text("Typ: " + book.type)
+                var divAuthors=nextDiv.children('#authors').text("Autor: " );
+                authorsList(book.authors, divAuthors);
+                nextDiv.children('#publisher').text("Wydawca: " + book.publisher);
+                nextDiv.children('#isbn').text("Isbn: " + book.isbn);
+                nextDiv.children('#type').text("Typ: " + book.type);
             })
-        };
+        }
         nextDiv.toggle()
     });
 
+    function authorsList(authors, nextDiv) {
+        for (const author of authors) {
+            var newA = newAElementAuthor(author);
+            nextDiv.append(newA)
+        }
+    }
+
+    function newAElementAuthor(author) {
+        return $("<a data-id=" + author.id + ">" + author.firstName + " " + author.lastName + " </a>");
+    }
+
     $('button').on('click', function () {
-        event.preventDefault()
+        event.preventDefault();
         var book = {
             "isbn": $('form input#isbn')[0].value,
             "title": $('form input#title')[0].value,
             "publisher": $('form input#publisher')[0].value,
             "type": $('form input#type')[0].value,
             "author": $('form input#author')[0].value
-        }
+        };
         makeAjaxCall('POST', book, function () {
             location.reload();
         })
-    })
+    });
 
     $(mainDiv).on('click', 'a#delete', function () {
-        event.stopPropagation()
-        event.preventDefault()
-        event.stopImmediatePropagation()
+        event.stopPropagation();
+        event.preventDefault();
+        event.stopImmediatePropagation();
 
         makeAjaxCall('DELETE', $(this).parent().data('id'), function () {
             location.reload();
